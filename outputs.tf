fd2db1e7ef534406df9d7474b54759f3faba3fcc
BartTho <61376948+BartTho@users.noreply.github.com>
2025-10-28 21:05:48 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,1 @@
-# outputs.tf
-output "id" {
-  description = "Instance VM ID"
-  value       = proxmox_vm_qemu.vm.id
-}
 
-output "public_ipv4" {
-  description = "Instance Public IPv4 Address"
-  value       = proxmox_vm_qemu.vm.default_ipv4_address
-}
