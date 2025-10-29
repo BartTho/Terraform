baeaa0b1f3b40b8efa9261f6067c5068a99a3f78
BartTho <61376948+BartTho@users.noreply.github.com>
2025-10-29 14:41:25 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.23.1"
+    }
+  }
+}
+
 provider "docker" {
-  host = "npipe:////./pipe/docker_engine"
+  host = "tcp://192.168.1.20:2375"
 }
