7ddc79ab4219f1953b17abdf8b8fddb35cd55911
BartTho <61376948+BartTho@users.noreply.github.com>
2025-10-29 14:42:48 +0100

    Replace Python containers with Ubuntu webserver
    Replaced Python container resources with a webserver container using the latest Ubuntu image.

--- main.tf
+++ main.tf
@@ -1,47 +1,17 @@
-resource "docker_image" "python_image" {
-  name = var.image
+# Creating a Docker Image ubuntu with the latest as the Tag.
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
 }
 
-resource "docker_container" "python_container_1" {
-  name  = var.container_name_1
-  image = docker_image.python_image.latest
-
-  volumes {
-    container_path = "/app"
-    host_path      = "${path.module}/app1"
-  }
-
-  ports {
-    internal = 5000
-    external = 5001
-  }
-
-  command = ["python", "/app/script.py"]
-}
-
-resource "docker_container" "python_container_2" {
-  name  = var.container_name_2
-  image = docker_image.python_image.latest
-
-  volumes {
-    container_path = "/app"
-    host_path      = "${path.module}/app2"
-  }
-
-  ports {
-    internal = 5000
-    external = 5002
-  }
-
-  command = ["python", "/app/script.py"]
-}
-
-resource "local_file" "app1_script" {
-  filename = "${path.module}/app1/script.py"
-  content  = var.script_content
-}
-
-resource "local_file" "app2_script" {
-  filename = "${path.module}/app2/script.py"
-  content  = var.script_content
+# Creating a Docker Container using the latest ubuntu image.
+resource "docker_container" "webserver" {
+  image             = docker_image.ubuntu.latest
+  name              = "terraform-docker-test"
+  must_run          = true
+  publish_all_ports = true
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
 }
